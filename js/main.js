3c4de1adaec40049d64e1ecaeac143618d476ef5
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-27 00:15:09 +0330

    Create 'about' section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function (){
 
     $('#menu').click(function(){
         $(this).toggleClass('fa-times');
@@ -9,5 +9,23 @@ $(document).ready(function () {
 
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
+
+        if($(window).scrollTop() > 60){
+            $('header .header-2').addClass('header-active');
+        }else{
+            $('header .header-2').removeClass('header-active');
+        }
+
+        $('section').each(function(){
+            let height = $(this).height();
+            let top = $(window).scrollTop();
+            let offset = $(this).offset().top - 200;     
+            let id = $(this).attr('id');
+
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`href="#${id}"`).addClass('active');
+            }
+        });
     });
-})
+});
